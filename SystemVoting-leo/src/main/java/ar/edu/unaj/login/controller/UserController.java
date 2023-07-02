655426e23f9efcbd9a6013894396a87af8877067
Alanisgas <gaston.alanis@gmail.com>
2023-07-01 22:03:22 -0300

    fix the sending of mail when a user is created

---
+++ SystemVoting-leo/src/main/java/ar/edu/unaj/login/controller/UserController.java
@@ -0,0 +1,78 @@
+package ar.edu.unaj.login.controller;
+
+import ar.edu.unaj.login.model.PoliticalParty;
+import ar.edu.unaj.login.model.User;
+import ar.edu.unaj.login.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/Users")
+@Api (value = "User")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @CrossOrigin(origins = "http://localhost:4200")
+    @ApiOperation(value = "create User",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully"),
+            @ApiResponse(code = 406, message = "You are not able to create user")})
+    @PostMapping(path = "/create-User")
+    public ResponseEntity<Void> createUser(@RequestBody User user) throws Exception {
+        Optional<User> userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            return new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            User newUser = userService.addUser(user);if (newUser != null) {
+                // Enviamos el correo electrónico de bienvenida al nuevo usuario.
+                if (userService.sendWelcomeMail(newUser)) {
+                    return new ResponseEntity<Void>(HttpStatus.CREATED);
+                } else {
+                    // Si el envío de correo falla, puedes decidir cómo manejarlo.
+                    return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
+        }
+    }
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PutMapping
+    public ResponseEntity<User> updateUsers(@RequestBody User user){
+        User userUpdate = userService.updateUser(user);
+        return  new ResponseEntity(this.userService.updateUser(user),HttpStatus.CREATED);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping
+    public List<User> getStudets(){
+        return  userService.findAllUser();
+    }
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("User/{UserId}")
+    public Optional<User> getUserById(@PathVariable String UserId){
+        return userService.getUserById(UserId);
+    }/*
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("Users/{file}")
+    public List<User> findStudenfile(@PathVariable int file){
+        return  UserService.getFile(file);
+    }*/
+    @CrossOrigin(origins = "http://localhost:4200")
+    @DeleteMapping("User/{UserIds}")
+    public String deleteUser(@PathVariable String UserIds){
+        return  userService.deleteUser(UserIds);
+    }
+}
---
+++ SystemVoting-leo/src/main/java/ar/edu/unaj/login/model/PoliticalParty.java
@@ -0,0 +1,20 @@
+package ar.edu.unaj.login.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+
+@Document (collection = "politicalParty")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PoliticalParty {
+    @Id
+    private String idPP;
+    private String namePP;
+    private List<String> personal;
+}
